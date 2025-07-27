65bd093f51d452ddefd13b5ed1bd84706c20a8a1
sena <gulozseven@gmail.com>
2025-07-27 23:26:25 +0300

    Librarian -will be revisited

--- src/Library/Books/Book.java
+++ src/Library/Books/Book.java
@@ -2,6 +2,8 @@ package Library.Books;
 import Library.People.Author;
 import Library.enums.Status;
 
+import java.util.Objects;
+
 // Author classını import etmediğim için kızmıyor?
 
 
@@ -12,12 +14,89 @@ public abstract class Book {
     private Status status;
     private int edition;
     private String date_of_purchase;
+    private double price;
 
-    public abstract void get_title();
-    public abstract void get_author();
-    public abstract void change_owner();
-    public abstract void get_owner();
+    public String getTitle() {
+        return name;
+    };
+
+    public abstract void getAuthor();
+    public abstract void changeOwner();
+    public abstract void getOwner();
     public abstract void display();
-    public abstract void update_status();
+    public abstract void updateStatus();
 
+    public Book(int book_ID, Author author, String name, Status status, int edition, String date_of_purchase, double price) {
+        this.book_ID = book_ID;
+        this.author = author;
+        this.name = name;
+        this.status = status;
+        this.edition = edition;
+        this.date_of_purchase = date_of_purchase;
+        this.price = price;
+    }
+
+    public int getBook_ID() {
+        return book_ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public int getEdition() {
+        return edition;
+    }
+
+    public String getDate_of_purchase() {
+        return date_of_purchase;
+    }
+
+    public void setBook_ID(int book_ID) {
+        this.book_ID = book_ID;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void setEdition(int edition) {
+        this.edition = edition;
+    }
+
+    public void setDate_of_purchase(String date_of_purchase) {
+        this.date_of_purchase = date_of_purchase;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return book_ID == book.book_ID && edition == book.edition && Objects.equals(author, book.author) && Objects.equals(name, book.name) && status == book.status && Objects.equals(date_of_purchase, book.date_of_purchase);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(book_ID, author, name, status, edition, date_of_purchase);
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
---
+++ src/Library/People/Librarian.java
@@ -0,0 +1,68 @@
+package Library.People;
+import Library.enums.Status;
+//librarian da bir person degil midirr?  - extends people, Book dersek
+
+import Library.Books.Book;
+
+import java.util.Set;
+
+public class Librarian{
+    private String name;
+    private String password;
+
+    public Librarian(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean searchBook(Set<Book> books, String targetTitle) {
+        for (Book book : books) {
+            if (book.getTitle().equalsIgnoreCase(targetTitle)) {
+                System.out.println(targetTitle + " is found.");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean verifyMember(Person reader, String targetMember) {
+        if (!reader.getName().equalsIgnoreCase(targetMember)) {
+            System.out.println(targetMember + "is not a member.");
+            return false;
+        } else {
+            System.out.println(targetMember + "is a member.");
+            return true;
+        }
+    }
+
+    public void issueBook(Book book, Person borrower, String date) {
+        if (searchBook(Set < book >, book.getTitle())) {
+            if (verifyMember(borrower, borrower.getName())) {
+                if (book.getStatus() == Status.AVAILABLE) {
+                    book.changeOwner();
+                    book.updateStatus();
+                    book.setDate_of_purchase(date);
+
+                }
+            }
+        }
+    }
+
+
+}
